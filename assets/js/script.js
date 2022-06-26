c78906168537aba84957b62a8a298d7de757b597
Ron Dronet <ron@dronetdevdesign.com>
2022-06-26 13:02:42 -0500

    revised JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,31 @@
+var inputs = {};
+var inputElement = document.querySelector("#input-text-area");
+
+// moment.js current date:
 var todaysDate = moment().format('dddd, MMMM Do YYYY').toString();
 document.getElementById("current-day").innerHTML = todaysDate;
 // console.log(todaysDate);
+
+// save calendar input-list-element:
+var saveInput = function() {
+  localStorage.setItem("inputElement", JSON.stringify(inputElement));
+};
+
+// saveBtn targeting textarea:
+$("#time-block-groups .saveBtn").click(function() {
+  var inputText = $("#time-block-groups .input-list-element").val();
+
+  saveInput();
+
+  console.log(inputText);
+});
+
+
+$(".input-list-element").on( "click", function() {
+  console.log($(this).text());
+
+  saveInput();
+});
+
+
 
