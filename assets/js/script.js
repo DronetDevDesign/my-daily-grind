4165c60beae06e974436a5f3e86fd1dcb7af8cdf
Ron Dronet <ron@dronetdevdesign.com>
2022-06-29 13:47:49 -0500

    revised JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,9 @@ document.getElementById("current-day").innerHTML = todaysDate;
 
 // QUESTION: => How do I get the hour and save it with the text? 
 // The ".hour" class isn't pulling the hour or time into local storage or into the console.
-// The ".hour" class was place in the .children() with the ".input-schedule" class in BLOCK 2. 
+// The ".hour" class was place in the .children() with the ".input-schedule" class in BLOCK 2.
+// BLOCK 3 - shows the code that works without the ".hour" class. 
+// BLOCK 3 - returns the text in console.log and localStorage but without the ".hour". 
 
 // BLOCK 1 -
 // save calendar input-text element:
@@ -18,8 +20,8 @@ var saveInput = function(input) {
   localStorage.setItem("inputText", JSON.stringify(input));
 };
 
-// BLOCK 2 -
-// save button that stores text into localStorage:
+BLOCK 2 -
+I am trying to save the ".hour" AND ".input-schedule" together in localStorage.
 $(".saveBtn").on("click", function() {
   var inputText = $(this).parent(".input-group").children(".hour, .input-schedule").val();
     if (inputText) {
@@ -28,30 +30,42 @@ $(".saveBtn").on("click", function() {
     console.log(inputText);
 });
 
+// BLOCK 3 - This block returns the text from the textarea only:
+// $(".saveBtn").on("click", function() {
+//   var inputText = $(this).parent(".input-group").children(".input-schedule").val();
+//     if (inputText) {
+//     saveInput(inputText);
+//     }
+//     console.log(inputText);
+// });
+
 // =========================================================================================
 // =========================================================================================
 
 
 
 // get the current time function to run past - present - future:
-var getTime = function(currentTime) {
-  var time = $(currenTime).val()
-    .find(".hour")
-    .text()
-    .trim();
+// var getTime = function(currentTime) {
+//   var currentTime = $(this).parent(".input-group").children(".hour").val();
+//   var time = $(currentTime)
+//     .find(".hour")
+//     .text()
+//     .trim();
+
+//     console.log(time);
 
 // use moment.js to set the time:
-var clock = moment(time, "L").set("hour", 9);
+// var clock = moment(time, "L").set("hour", 9);
 
 // remove pas - future when time is present time:
-$(currentTime).removeClass("past future");
+// $(currentTime).removeClass("past future");
 
 // conditional statement to toggle through the states during timesblocks:
-  if (moment().isAfter(clock)) {
-    $(currenTime).addClass("present");
-  } else if (Math.abs(moment().diff(time, "hours")) <= 1) {
-    $(currentTime).addClass("future");
-  }
-};
+//   if (moment().isAfter(clock)) {
+//     $(currenTime).addClass("present");
+//   } else if (Math.abs(moment().diff(time, "hours")) <= 1) {
+//     $(currentTime).addClass("future");
+//   }
+// };
 
 
