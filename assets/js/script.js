2df0e98a14c3a9b6d048656cb49a8e47877ad855
Ron Dronet <ron@dronetdevdesign.com>
2022-06-29 17:59:42 -0500

    Deleted comments in files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,12 @@
 // moment.js current date displayed inside of header:
 var todaysDate = moment().format('dddd, MMMM Do YYYY').toString();
 document.getElementById("current-day").innerHTML = todaysDate;
-// console.log(todaysDate);
+
 
 var getInput = function() {
   JSON.parse(localStorage.getItem("inputText"));
 };
-// ==========================================================================================
-// ===================== THESE BLOCKS WITHIN THESE LINES ====================================
 
-// QUESTION: => How do I get the hour and save it with the text? 
-// The ".hour" class isn't pulling the hour or time into local storage or into the console.
-// The ".hour" class was place in the .children() with the ".input-schedule" class in BLOCK 2.
-// BLOCK 3 - shows the code that works without the ".hour" class. 
-// BLOCK 3 - returns the text in console.log and localStorage but without the ".hour". 
-
-// BLOCK 1 -
 // save calendar input-text element:
 var saveInput = function(input) {
   localStorage.setItem("inputText", JSON.stringify(input));
@@ -25,8 +16,6 @@ var getTime = function(time) {
   localStorage.setItem("exactHour", JSON.stringify(time));
 };
 
-// BLOCK 2 -
-// I am trying to save the ".hour" AND ".input-schedule" together in localStorage.
 $(".saveBtn").on("click", function() {
   var inputText = $(this)
     .parent(".input-group")
@@ -36,7 +25,7 @@ $(".saveBtn").on("click", function() {
       if (inputText) {
       saveInput(inputText);
     }
-    console.log(inputText);
+    // console.log(inputText);
 
   var exactHour = $(this)
     .parent(".input-group")
@@ -46,22 +35,8 @@ $(".saveBtn").on("click", function() {
       if (exactHour) {
       getTime(exactHour);
     }
-    console.log(exactHour);    
+    // console.log(exactHour);    
 });
-
-// BLOCK 3 - This block returns the text from the textarea only:
-// $(".saveBtn").on("click", function() {
-//   var inputText = $(this).parent(".input-group").children(".input-schedule").val();
-//     if (inputText) {
-//     saveInput(inputText);
-//     }
-//     console.log(inputText);
-// });
-
-// =========================================================================================
-// =========================================================================================
-
-
 
 // get the current time function to run past - present - future:
 var getTime = function(currentTime) {
@@ -70,7 +45,7 @@ var getTime = function(currentTime) {
     .text()
     .trim();
 
-    console.log(time);
+    // console.log(time);
 
 // use moment.js to set the time:
 var clock = moment(time).set(1, "hours");
